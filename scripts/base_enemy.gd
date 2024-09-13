a9a4d0582a9415aa5b1c92f3e77f6eaf4722ae6f
snk-jan <kir.j4n@gmail.com>
2024-09-13 14:04:26 +0200

    base_enemy implementation
    Enemy can pursue the player, stop pursuing the player and return to his starting position

---
+++ scripts/base_enemy.gd
@@ -0,0 +1,98 @@
+extends Node2D
+
+# customizable variables
+@export var dead = false
+@export var respawnable = true
+@export var max_health_points = 100
+@export var armor = 20
+@export var max_stamina = 100
+@export var can_jump = false
+@export var speed = 10
+@export var sin_amount = 10
+@export var return_to_start_after = 5
+
+# variables
+var current_health_points = max_health_points
+var current_stamina = max_stamina
+var return_to_start = return_to_start_after
+var pursuing = false
+var current_target = null
+var target_position = null
+var start_position = null
+var start_rotation = false
+
+# one time call after everything is initialized
+func _ready() -> void:
+	start_rotation = $RigidBody2D/AnimatedSprite2D.flip_h
+	start_position = position
+
+# game loop function
+func _process(delta: float) -> void:
+	
+	# vvvvv movement vvvvv
+	
+	# sets target position
+	if(pursuing):
+		target_position = current_target.position
+		if(is_close_to(position.x, target_position.x, 0.05)): return
+		return_to_start = return_to_start_after
+	elif(return_to_start >= 0):
+		return_to_start -= delta
+		target_position = null
+	elif(!is_close_to(position.x, start_position.x, 0.05)):
+		target_position = start_position
+		
+		
+	# enemy has target
+	if(target_position != null):
+	
+		# move right
+		if(target_position.x > position.x):
+			$RigidBody2D/AnimatedSprite2D.flip_h = true
+			$RigidBody2D/detection/CollisionPolygon2D.rotation_degrees = 180
+			position.x += delta * speed
+			
+		# move left
+		else :
+			$RigidBody2D/AnimatedSprite2D.flip_h = false
+			$RigidBody2D/detection/CollisionPolygon2D.rotation_degrees = 0
+			position.x -= delta * speed
+			
+		# stop movement if position is close to target
+		if(is_close_to(position.x, target_position.x, 0.05)):
+			if(target_position == start_position && $RigidBody2D/AnimatedSprite2D.flip_h != start_rotation):
+				flip_rotation()
+			target_position = null
+	
+	pass
+	
+# signal when player enters detection area -> start following player
+func _on_detection_body_entered(body: Node2D) -> void:
+	if(check_line_if_sight(body)): 
+		pursuing = true
+		current_target = body
+	pass
+
+# signal when player leaves pursuing area -> stop following player
+func _on_pursuing_radius_body_exited(body: Node2D) -> void:
+	if(body == current_target):
+		pursuing = false
+		current_target = null
+	pass
+
+# check line of sight to a node
+func check_line_if_sight(body: Node2D) -> bool:
+	# TODO: implement check
+	return true
+
+# flips the rotation of sprite and detection polygon
+func flip_rotation() -> void:
+	$RigidBody2D/AnimatedSprite2D.flip_h = !$RigidBody2D/AnimatedSprite2D.flip_h
+	$RigidBody2D/detection/CollisionPolygon2D.rotation_degrees = abs($RigidBody2D/detection/CollisionPolygon2D.rotation_degrees - 180)
+	
+
+# checks if two values are close to each other with set delta	
+func is_close_to(value: float, close: float, delta: float) -> bool:
+	var up = close + delta
+	var down = close - delta
+	return (value <= up && value >= down)
