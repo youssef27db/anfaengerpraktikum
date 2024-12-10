e3fdece71a9ae543978b996921b526bbf46647c1
Snehann Sritharan <snehann.sritharan@stud.uni-heidelberg.de>
2024-12-10 19:56:05 +0100

    NavigationManager Zwischenschritt 1

---
+++ scripts/Door.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Door : Area2D
+{
+    [Export]
+    public string DestinationLevelTag { get; set; }
+
+    [Export]
+    public string DestinationDoorTag { get; set; }
+
+    [Export]
+    public string SpawnDirection { get; set; } = "up";
+
+    private Node spawn;
+
+    public override void _Ready()
+    {
+        spawn = GetNode("Spawn");
+    }
+
+	private void OnPlayerBodyEntered(Node body){
+		if (body is Player player)
+		{
+		}
+	}
+}
