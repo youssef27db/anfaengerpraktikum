b28695be2d5c7c2242e49c38b56bbcc1fd33cfe5
youssef27db <youssefdaoudi275@gmail.com>
2025-07-15 18:03:16 +0200

    Alle unittests für Player

--- tests/PlayerTest2.cs
+++ tests/PlayerTest2.cs
@@ -37,6 +37,7 @@ public class PlayerTest2
         // Test for W
         await TestMovement(Key.W, Vector2.Up);
 
+        await Setup();
     }
 
     [TestCase]
@@ -46,6 +47,7 @@ public class PlayerTest2
         // Test for A
         await TestMovementApprox(Key.A, Vector2.Left);
 
+        await Setup();
     }
 
     [TestCase]
@@ -55,6 +57,7 @@ public class PlayerTest2
         // Test for D
         await TestMovementApprox(Key.D, Vector2.Right);
 
+        await Setup();
     }
 
     [TestCase]
@@ -62,7 +65,11 @@ public class PlayerTest2
     public async Task TestDashWithNoDirectionMovement()
     {
         // Test L-Shift for dash if no direction is pressed
-        await TestMovementApprox(Key.Shift, Vector2.Zero);
+        Player.Position = new Vector2(120, 288);
+
+        await TestMovement(Key.Shift, Vector2.Zero);
+
+        await Setup();
     }
 
     [TestCase]
@@ -75,7 +82,7 @@ public class PlayerTest2
         await Runner.SimulateFrames(1, 1);
         await TestMovementApprox(Key.Shift, Vector2.Left);
 
-
+        await Setup();
     }
 
     [TestCase]
@@ -87,6 +94,8 @@ public class PlayerTest2
         Runner.SimulateKeyPress(Key.D);
         await Runner.SimulateFrames(1, 1);
         await TestMovementApprox(Key.Shift, Vector2.Right);
+
+        await Setup();
     }
 
     [TestCase]
@@ -98,6 +107,8 @@ public class PlayerTest2
         Runner.SimulateKeyPress(Key.W);
         await Runner.SimulateFrames(1, 1);
         await TestMovementApprox(Key.Shift, Vector2.Up);
+
+        await Setup();
     }
 
     [TestCase]
@@ -105,11 +116,13 @@ public class PlayerTest2
     public async Task TestDashWithLightAttackMovement()
     {
         // Test L-Shift for dash if light attack is pressed
-        Player.Position = new Vector2(150, 288); // Reset position to avoid interference 
+        Player.Position = new Vector2(120, 288); // Reset position to avoid interference 
 
         Runner.SimulateMouseButtonPress(MouseButton.Left);
-        await Runner.SimulateFrames(1, 1);
+        await Runner.SimulateFrames(1, 100);
         await TestMovementApprox(Key.Shift, Vector2.Zero);
+
+        await Setup();
     }
 
     [TestCase]
@@ -120,20 +133,25 @@ public class PlayerTest2
         Player.Position = new Vector2(120, 288); // Reset position to avoid interference
 
         Runner.SimulateMouseButtonPress(MouseButton.Right);
-        await Runner.SimulateFrames(1, 1);
+        await Runner.SimulateFrames(1, 100);
         await TestMovementApprox(Key.Shift, Vector2.Zero);
+
+        await Setup();
     }
 
     [TestCase]
     [RequireGodotRuntime]
     public async Task TestHeavyAttack()
     {
+        Player.Position = new Vector2(120, 288);
         // Test heavy attack with right mouse button
 
         Runner.SimulateMouseButtonPress(MouseButton.Right);
-        await Runner.SimulateFrames(1, 1);
-        
+        await Runner.SimulateFrames(1, 100);
+
         Assertions.AssertThat(Player.IsAttacking()).IsTrue();
+
+        await Setup(); // Reset player state after attack
     }
 
     [TestCase]
@@ -142,10 +160,14 @@ public class PlayerTest2
     {
         // Test light attack with right mouse button
 
+        Player.Position = new Vector2(120, 288); // Reset position to avoid interference
+
         Runner.SimulateMouseButtonPress(MouseButton.Left);
-        await Runner.SimulateFrames(1, 1);
-        
+        await Runner.SimulateFrames(1, 100);
+
         Assertions.AssertThat(Player.IsAttacking()).IsTrue();
+        
+        await Setup();
     }
 
     [TestCase]
@@ -155,9 +177,104 @@ public class PlayerTest2
         // Test block with space key
 
         Runner.SimulateKeyPress(Key.Space);
-        await Runner.SimulateFrames(1, 1);
-        
+        await Runner.SimulateFrames(5, 100);
         Assertions.AssertThat(Player.IsBlocking()).IsTrue();
+
+        await Setup();
+    }
+
+    [TestCase]
+    [RequireGodotRuntime]
+    public async Task TestMaxHeal()
+    {
+        // Test if player has maximum health
+        float initialHealth = PlayerStats.Instance.GetCurrentHealth();
+
+        PlayerStats.Instance.SetCurrentHealth(50.0f);
+        Player.MaxHeal();
+        await Runner.SimulateFrames(1, 100);
+        Assertions.AssertThat(PlayerStats.Instance.GetCurrentHealth()).IsEqual(initialHealth);
+        await Setup();
+    }
+
+    [TestCase]
+    [RequireGodotRuntime]
+    public async Task TestTakeDMG()
+    {
+        // Test if player has maximum health
+        Spike Spike = GD.Load<PackedScene>("res://scenes/spike.tscn").Instantiate<Spike>();
+        Runner.Scene().AddChild(Spike);
+        float initialHealth = PlayerStats.Instance.GetCurrentHealth();
+
+        Damage Damage = Spike.GetDamage();  // Get 10 damage from Spike
+        Player.TakeDamage(Damage);
+
+        await Runner.SimulateFrames(1, 1);
+        Assertions.AssertThat(PlayerStats.Instance.GetCurrentHealth()).IsEqual(initialHealth - 10.0f);
+        await Setup();
+    }
+
+    [TestCase]
+    [RequireGodotRuntime]
+    public async Task TestTakeDMGWhileBlocking()
+    {
+        // Test if player has maximum health
+        BaseEnemy BaseEnemy = GD.Load<PackedScene>("res://scenes/base_enemy.tscn").Instantiate<BaseEnemy>();
+        Runner.Scene().AddChild(BaseEnemy);
+        PlayerStats.Instance.SetCurrentHealth(100.0f); // Set initial health
+
+        Runner.SimulateKeyPress(Key.Space); // Simulate blocking
+        await Runner.SimulateFrames(5, 100);
+        Damage DMG = new Damage(50.0f, 0, Vector2.Zero, BaseEnemy);
+        Player.TakeDamage(DMG);
+
+        Assertions.AssertThat(PlayerStats.Instance.GetCurrentHealth()).IsEqual(100.0f); // Health should remain the same due to blocking
+        await Setup();
+    }
+
+    [TestCase]
+    [RequireGodotRuntime]
+    public async Task TestRegenerateStamina()
+    {
+        // Test stamina regeneration
+        PlayerStats.Instance.SetStamina(50.0f); // Set initial stamina
+        float staminaRegenRate = 10f;
+        float delta = 0.5f; // Simulate half a second
+
+        Player.RegenerateStamina(staminaRegenRate, delta);
+        await Runner.SimulateFrames(5, 100);
+        
+        Assertions.AssertThat(PlayerStats.Instance.GetStamina()).IsEqual(100.0f); // Assuming max stamina is 100
+
+        await Setup();
+    }
+
+    [TestCase]
+    [RequireGodotRuntime]
+    public async Task TestUseBloodVial()
+    {
+        // Test using a blood vial
+        PlayerStats.Instance.SetCurrentHealth(50.0f); // Set initial health
+
+        await Runner.AwaitIdleFrame();
+        Runner.SimulateKeyPress(Key.Q); // Simulate using blood vial
+        Assertions.AssertThat(PlayerStats.Instance.GetCurrentHealth()).IsEqual(50.0f);
+
+        await Setup();
+    }
+
+    [TestCase]
+    [RequireGodotRuntime]
+    public async Task TestUseBloodVialWithMaxHealth()
+    {
+        // Test using a blood vial when health is already at maximum
+        PlayerStats.Instance.SetCurrentHealth(100.0f); // Set initial health
+
+        await Runner.AwaitIdleFrame();
+        Runner.SimulateKeyPress(Key.Q); // Simulate using blood vial
+        Assertions.AssertThat(PlayerStats.Instance.GetCurrentHealth()).IsEqual(100.0f);
+
+        await Setup();
     }
 
     private async Task TestMovement(Key key, Vector2 expectedDirection) //KeyboardInput
@@ -184,7 +301,7 @@ public class PlayerTest2
             Semaphore.Release(); // release the semaphore when task is done
         }
     }
-    
+
     private async Task TestMovementApprox(Key key, Vector2 expectedDirection) //KeyboardInput
     {
         await Semaphore.WaitAsync(); // wait for semaphore
